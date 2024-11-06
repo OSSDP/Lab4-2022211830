f6f995b2821a82ae192b058ad9491c1b17277870
kathy40405 <2752109281@qq.com>
2024-11-06 15:13:37 +0800

    lab4.1.4

---
+++ src/main/java/Solution4.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+
+/**
+ * @description:
+ *
+ * 给定一个无序的数组 nums，返回 数组在排序之后，相邻元素之间最大的差值 。如果数组元素个数小于 2，则返回 0 。
+ *
+ * 您必须编写一个在「线性时间」内运行并使用「线性额外空间」的算法。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [3,6,9,1]
+ * 输出: 3
+ * 解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
+ * 示例 2:
+ *
+ * 输入: nums = [10]
+ * 输出: 0
+ * 解释: 数组元素个数小于 2，因此返回 0。
+ *
+ *
+ * 提示:
+ *
+ * 1 <= nums.length <= 105
+ * 0 <= nums[i] <= 109
+ *
+ */
+public class Solution4 {
+    public int maximumGap(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return 0;
+        }
+
+        int n = nums.length;
+        int maxVal = Arrays.stream(nums).max().getAsInt();
+        int minVal = Arrays.stream(nums).min().getAsInt();
+
+        if (maxVal == minVal) {
+            return 0;
+        }
+
+        int bucketSize = Math.max(1, (maxVal - minVal) / (n - 1));
+        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+
+        int[][] buckets = new int[bucketCount][2];
+        for (int i = 0; i < bucketCount; i++) {
+            buckets[i][0] = Integer.MAX_VALUE;
+            buckets[i][1] = Integer.MIN_VALUE;
+        }
+
+        for (int num : nums) {
+            int bucketIndex = (num - minVal) / bucketSize;
+            buckets[bucketIndex][0] = Math.min(buckets[bucketIndex][0], num);
+            buckets[bucketIndex][1] = Math.max(buckets[bucketIndex][1], num);
+        }
+
+        int ret = 0;
+        int previousMax = buckets[0][1];
+
+        for (int i = 1; i < bucketCount; i++) {
+            if (buckets[i][0] == Integer.MAX_VALUE) continue;
+            ret = Math.max(ret, buckets[i][0] - previousMax);
+            previousMax = buckets[i][1];
+        }
+
+        return ret;
+    }
+}
